19cf28fe95dd7257b14d1fddaa3f6bb09054407b
CoderSivasakthi <sivasakthi83974@gmail.com>
2024-08-02 01:03:48 +0530

    wrapper class was added

---
+++ Classes/WrapperClassDemo.java
@@ -0,0 +1,38 @@
+package Classes;
+
+import java.util.ArrayList;
+
+public class WrapperClassDemo {
+    public static void main(String args[]){
+
+        /* WrapperClass which is used to convert the primitive datatype into WrapperClassObjects.
+            to convert primitive datatypes into objects autoboxing
+            and reverse the autoboxing to unautoboxing the object to primitive datatypes ;
+
+        */
+
+
+        //Autoboxing
+//        char ch ='a';
+//
+//        Character a = ch;
+//
+//        ArrayList<Integer> al = new ArrayList<Integer>();
+//        al.add(20);
+//
+//        System.out.println(al);
+
+        //UnAutoboxing
+
+        Character ch = 'a';
+        char a = ch;
+        System.out.println(a);
+        if(ch instanceof Character){
+            System.out.println("char value "+a);
+        }
+        else{
+            System.out.println("NO"+a);
+        }
+        System.out.println(ch);
+    }
+}
